2f4e462a376b10029f27aed559759ce2d00405b2
meliya-e <eredzepovamelya@gmail.com>
2024-11-18 00:01:42 +0300

    done

--- 20241111/3/prog.py
+++ 20241111/3/prog.py
@@ -1,49 +1,56 @@
 class Maze:
     def __init__(self, N):
         self.N = N
-        self.maze = [['█' for _ in range(N)] for _ in range(N)]
-
-    def __getitem__(self, item):
-        if isinstance(item, slice):
-            x0, y0, x1, y1 = item.start[0], item.start[1], item.stop[0], item.stop[1]
-            return self._is_reachable(x0, y0, x1, y1)
-        else:
-            return self.maze[item[0]][item[1]]
-
-    def __setitem__(self, item, value):
-        if isinstance(item, slice):
-            x0, y0, x1, y1 = item.start[0], item.start[1], item.stop[0], item.stop[1]
-            
-            for x in range(min(x0,x1), max(x0,x1)+1):
-                for y in range(min(y0,y1), max(y0,y1)+1):
-                    if x == x0 and abs(y-y0) == 1:
-                        self.maze[x][y] = value
-                    if y == y0 and abs(x - x0) == 1:
-                        self.maze[x][y] = value
-        else:
-            self.maze[item[0]][item[1]] = value
-
-    def _is_reachable(self, x0, y0, x1, y1):
-        visited = set()
-        queue = [(x0, y0)]
+        self.field = [['█' for _ in range(N * 2  + 1)] for _ in range(N * 2  + 1)]
         
-        while queue:
-            x, y = queue.pop(0)
-            if (x, y) == (x1, y1):
-                return True
-            visited.add((x, y))
-            
-            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < self.N and 0 <= ny < self.N and \
-                   self.maze[nx][ny] == '·' and (nx, ny) not in visited:
-                    queue.append((nx, ny))
-                    
+        for i in range(1, N * 2  + 1, 2):
+            for j in range(1, N * 2  + 1, 2):
+                self.field[i][j] = '.'
+    
+    def __str__(self):
+        return '\n'.join(''.join(row) for row in self.field)
+    
+    def __getitem__(self, key):
+        x0, y0, x1, y1 = key[0], key[1].start, key[1].stop, key[2]
+        if x0 == x1:  
+            return self.check_path(x0 * 2 + 1, min(y0, y1) * 2 + 1, x0 * 2 + 1, max(y0, y1)* 2 + 1)
+        elif y0 == y1: 
+            return self.check_path(min(x0, x1)* 2 + 1, y0 * 2 + 1, max(x0, x1)* 2 + 1, y0 * 2 + 1)
+        elif x0 < x1 and y0 < y1:
+            return self.check_path(x0 * 2 + 1, y0 * 2 + 1, x1* 2 + 1, y1 * 2 + 1)
         return False
 
+    def check_path(self, x0, y0, x1, y1):
+        visited = set()
+        return self.check_neighbors(x0, y0, x1, y1, visited)
 
-    def __str__(self):
-        return '\n'.join([''.join(row) for row in self.maze])
+    def check_neighbors(self, x, y, target_x, target_y, visited):
+        if (x, y) in visited:
+            return False
+        visited.add((x, y))
+        if (x, y) == (target_x, target_y):
+            return True
+        
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x + dx, y + dy
+            if self.field[ny][nx] == '·':
+                if self.check_neighbors(nx, ny, target_x, target_y, visited):
+                    return True
+        return False
+    
+    def __setitem__(self, key, value):
+        
+        x0, y0, x1, y1 = key[0], key[1].start, key[1].stop, key[2]
+        if x0 == x1: 
+            self.open_or_close(x0, min(y0, y1), x0, max(y0, y1), value)
+        elif y0 == y1: 
+            self.open_or_close(min(x0, x1), y0, max(x0, x1), y0, value)
+    
+    def open_or_close(self, x0, y0, x1, y1, value):
+        for x in range(x0 * 2 + 1 , (x1 * 2 + 2)  ):
+            for y in range(y0* 2 + 1, (y1 * 2 + 2) ):
+                self.field[y ][x ] = value
+                
 
 import sys
 exec(sys.stdin.read())
