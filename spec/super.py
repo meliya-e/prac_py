4e6e43bac42a713fdb996b080d9ace70bf5f6c64
meliya-e <eredzepovamelya@gmail.com>
2024-12-24 11:52:13 +0300

    ok

--- spec/super.py
+++
@@ -1,8 +0,0 @@
-def superposition(funmod, funseq):
-    funres = []
-    for f in funseq:
-        def fun(x, f = f):
-            return funmod(f(x))
-        funres.append(fun)
-    return funres
-
