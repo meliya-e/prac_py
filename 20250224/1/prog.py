73712e7a1d4783e93d20619156a8f9e4b0eb6f21
meliya-e <eredzepovamelya@gmail.com>
2025-03-02 16:53:09 +0300

    base functionality MUD from task1

---
+++ 20250224/1/prog.py
@@ -0,0 +1,69 @@
+import sys
+import cowsay
+
+class MUD:
+    def __init__(self):
+        self.field = [[None for _ in range(10)] for _ in range(10)]
+        self.player_position = (0, 0)
+
+    def move_player(self, direction):
+        x, y = self.player_position
+        if direction == 'up':
+            y = (y - 1) % 10
+        elif direction == 'down':
+            y = (y + 1) % 10
+        elif direction == 'left':
+            x = (x - 1) % 10
+        elif direction == 'right':
+            x = (x + 1) % 10
+        else:
+            print("Invalid command")
+            return
+
+        self.player_position = (x, y)
+        print(f"Moved to ({x}, {y})")
+        self.encounter(x, y)
+
+    def add_monster(self, x, y, hello):
+        if (x, y) == self.player_position:
+            print("cannot add monster to player's position")
+            return
+        old_mon = self.field[x][y] is not None
+        #if self.field[x][y] is not None:
+         #   print("Replaced the old monster")
+        self.field[x][y] = hello
+        print(f"Added monster to ({x}, {y}) saying {hello}")
+        if old_mon:
+            print("Replaced the old monster")
+
+    def encounter(self, x, y):
+        if self.field[x][y] is not None:
+            print(cowsay.cow(self.field[x][y]))
+
+    def process_cmd(self, command):
+        parts = command.split()
+        if not parts:
+            print("Invalid command")
+            return
+
+        if parts[0] in ['up', 'down', 'left', 'right']:
+            self.move_player(parts[0])
+        elif parts[0] == 'addmon' and len(parts) == 4:
+            try:
+                x, y = int(parts[1]), int(parts[2])
+                hello = parts[3]
+                self.add_monster(x, y, hello)
+            except ValueError:
+                print("Invalid arguments")
+        else:
+            print("Invalid command")
+
+game = MUD()
+if sys.stdin.isatty():
+    while True:
+        #command = input()
+        game.process_cmd(input())
+else:
+    for line in sys.stdin:
+        game.process_cmd(line.strip())
+
